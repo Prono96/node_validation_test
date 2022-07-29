cf5d2c11d1ecc6ab7b24de7ec43f82c127f41b6e
Promise <promiseprince01@gmail.com>
2022-07-29 15:02:41 +0100

    push to heroku

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.post('/validate-rule', (req, res) => {
 })
 
 // App Listening
-const port = 5000
+const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
