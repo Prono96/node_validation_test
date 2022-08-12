2dd5703033068647b00746a5dd66a479275fa029
Promise <promiseprince01@gmail.com>
2022-08-12 13:03:58 +0100

    updated the error fumctions

--- app.js
+++ app.js
@@ -8,19 +8,18 @@ const { object } = require("joi");
 app.use(bodyParser.json({extended: false}));
 
 
-
+// Get endpoint data
 const example = {
   "message": "My Rule-Validation API",
   "status": "success",
   "data": {
-    "name": "Amos Burton",
-    "github": "@amosburton",
-    "email": "amosburton@rocinantecrew.com",
+    "name": "Promise Prince",
+    "github": "@prono96",
+    "email": "pro96.com",
     "mobile": "08069920011",
-    "twitter": "@amosb"
+    "twitter": "@prono_96"
   }
 }
-
 
 // Get routers
 app.get('/', (req, res) => {
@@ -29,17 +28,43 @@ app.get('/', (req, res) => {
 
 // Post routers
 app.post('/validate-rule', (req, res) => {
+  const payload = req.body;
+//  Validating the payload and returning error
   const { error, value } = validateSchema(req.body);
   if(error) {
     return res.status(400).send({
       message : (error.message),
       status : 'error',
-      data : null
+      data: {
+        validation: {
+          error: true,
+          field: payload.rule.field,
+          field_value: payload.data,
+          condition: payload.rule.condition,
+          condition_value: payload.rule.condition_value
+        }
+      }
     });
   } 
   
-  console.log('you are logged in');
-  res.status(200).send('You are logged in');
+  
+  // If the payload is correct it will return this 
+  res.status(200).send(
+    {
+      message: "field missions successfully validated.",
+      status: "success",
+      data: {
+        validation: {
+          error: false,
+          field: payload.rule.field,
+          field_value: payload.data.missions,
+          condition: payload.rule.condition,
+          condition_value: payload.rule.condition_value
+        }
+      }
+    }
+
+  );
   
 })
 
--- validator.js
+++ validator.js
@@ -6,9 +6,30 @@ schema.validate(payload, { abortEarly: false});
 // Defining the Schema 
 const schema = Joi.object({
   rule: {
-    field: Joi.string().required().label("field is required"),
-    condition: Joi.string().required().label("condition is required"),
-    condition_value: Joi.number().required()
+    field: Joi.string()
+    .required()
+    .messages({
+      'string.base': `field should be an object.'`,
+      'string.empty': `field is required.`,
+      'any.required': `"a" is a required field`
+    }),
+
+    condition: Joi.string()
+    .required()
+    .messages({
+      'string.base': `condition should be an object.'`,
+      'string.empty': `condition is required.`,
+      'string.min': `"a" should have a minimum length of {#limit}`,
+      'any.required': `"a" is a required field`
+    }),
+    condition_value: Joi.number()
+    .required()
+    .messages({
+      'string.base': `condition_value should be a number.'`,
+      'string.empty': `condition_value is required.'`,
+      'string.min': `"a" should have a minimum length of {#limit}`,
+      'any.required': `"a" is a required field`
+    }),
   },
   data: {
     name: Joi.string().required(),
